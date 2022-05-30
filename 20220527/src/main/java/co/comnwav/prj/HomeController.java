9c6fcab37810a69a20c2cd520e9fd6d406782b24
SEO HYEONIL <101534943+comnwav@users.noreply.github.com>
2022-05-30 10:50:16 +0900

    ...

--- 20220527/src/main/java/co/comnwav/prj/HomeController.java
+++ 20220527/src/main/java/co/comnwav/prj/HomeController.java
@@ -17,12 +17,8 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		
 		return "home/home";
 	}
 	
--- 20220527/src/main/java/co/comnwav/prj/student/web/StudentController.java
+++ 20220527/src/main/java/co/comnwav/prj/student/web/StudentController.java
@@ -6,15 +6,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import co.comnwav.prj.student.service.StudentService;
+import co.comnwav.prj.vo.StudentVO;
 
 @Controller
 public class StudentController {
 	@Autowired
 	StudentService studentDao; //DAO 객체를 자동 주입
 	
-	@RequestMapping("/studentList.do")
+	@RequestMapping("/studentList.do") // 전체리스트 조회
 	public String studentList(Model model) {
 		model.addAttribute("students", studentDao.studentSelectList());
 		return "student/studentList";
+	}
+	
+	// 항상 annotation, method, return naming 을 일치시킨다
+	@RequestMapping("/studentSelect.do") // 한명의 데이터 조회
+	public String studentSelect(StudentVO vo, Model model) {
+		model.addAttribute("student", studentDao.studentSelect(vo));
+		return "student/studentSelect";
 	}
 }
